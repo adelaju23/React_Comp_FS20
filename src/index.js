ca3de09eac7386ebe8f5f25acd66314dbb455588
pyrschool <72523829+pyrschool@users.noreply.github.com>
2021-01-24 01:11:27 +0200

    Act based on current hour

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const h1Style = { color: "red", fontSize: "28", border: "2px solid black" };
-const greeting = "Hello";
+const h1Style = { color: "blue", fontSize: "28", border: "2px solid black" };
+let greeting = "Hello";
+
+const today = new Date();
+const time = today.getHours();
+
+if (time > 18) {
+  greeting = "Good night";
+  h1Style.color = "black";
+} else if (time > 12) {
+  greeting = "Good evening";
+  h1Style.color = "gray";
+} else {
+  greeting = "Good morning";
+  h1Style.color = "red";
+}
 
 ReactDOM.render(
   <div>
