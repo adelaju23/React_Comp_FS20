a80862530f73d75d362d1e57884cc433fcf234ed
pyrschool <72523829+pyrschool@users.noreply.github.com>
2021-01-24 16:56:34 +0200

    Use components folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
 
 // deactivate the styling and content for now.
 // We will restore it later
