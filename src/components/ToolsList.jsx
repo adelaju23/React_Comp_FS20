3257a8970ad07d2940357db30af483f8870fd007
pyrschool <72523829+pyrschool@users.noreply.github.com>
2021-01-24 17:15:58 +0200

    Maje the list look like cards

--- src/components/ToolsList.jsx
+++ src/components/ToolsList.jsx
@@ -2,11 +2,17 @@ import React from "react";
 
 function ToolsList() {
   return (
-    <ul>
-      <li>Server side: Nodejs</li>
-      <li>Client side: ReactJS</li>
-      <li>RDBMS: PostgreSQL</li>
-    </ul>
+    <div>
+      <h3>Nodejs</h3>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1920px-Node.js_logo.svg.png" />
+      <h4>Server, Javascript</h4>
+      <h3>ReactJS</h3>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" />
+      <h4>Client, Javascript, JSX</h4>
+      <h3>PostgreSQL</h3>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png" />
+      <h4>Server, RDBMS, SQL</h4>
+    </div>
   );
 }
 
